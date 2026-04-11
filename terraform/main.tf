1c871e3b78c954d37391fdc2de04684a2cdf07b4
hebrewElevenOne <129419484+hebrewElevenOne@users.noreply.github.com>
2026-04-12 00:33:20 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,31 @@ resource "aws_db_subnet_group" "db_group" {
   name       = "sideline-db-group"
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 }
+
+# Security Group for RDS (Allows 5432 from App Runner only)
+resource "aws_security_group" "rds_sg" {
+  name   = "sideline-rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Only allow traffic from within our VPC
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier           = "sideline-db"
+  instance_class       = "db.t4g.micro"
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "16.2"
+  username             = "adminuser"
+  password             = var.db_password # We will set this in GitHub Secrets
+  db_subnet_group_name = aws_db_subnet_group.db_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+}
+
