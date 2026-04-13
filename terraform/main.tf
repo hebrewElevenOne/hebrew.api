6abdca5c505987f3dda72b85dd64b881bf3dab99
hebrewElevenOne <129419484+hebrewElevenOne@users.noreply.github.com>
2026-04-13 22:05:43 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,7 +67,7 @@ resource "aws_iam_role" "ecs_execution_role" {
       Action = "sts:AssumeRole",
       Effect = "Allow",
       Principal = {
-        Service = "://amazonaws.com"
+        Service = "ecs-tasks.amazonaws.com"
       }
     }]
   })
@@ -87,7 +87,7 @@ resource "aws_iam_role" "ecs_infrastructure_role" {
       Effect = "Allow",
       Action = "sts:AssumeRole",
       Principal = {
-        Service = "://amazonaws.com"
+        Service = "ecs-tasks.amazonaws.com"
       }
     }]
   })
