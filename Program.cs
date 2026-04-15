4d45b76912282e5a6140e4f44e98dee45daa476e
hebrewElevenOne <129419484+hebrewElevenOne@users.noreply.github.com>
2026-04-15 21:19:23 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,36 @@ builder.Services.AddSwaggerGen(); // Adds the Swagger generator
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path.StartsWithSegments("/swagger"))
+    {
+        string authHeader = context.Request.Headers["Authorization"];
+
+        if (authHeader != null && authHeader.StartsWith("Basic "))
+        {
+            var encodedCredentials = authHeader.Substring(6).Trim();
+            var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCredentials)).Split(':');
+            
+            // GET FROM ENVIRONMENT VARIABLES
+            var envUser = Environment.GetEnvironmentVariable("SWAGGER_USER");
+            var envPass = Environment.GetEnvironmentVariable("SWAGGER_PASS");
+
+            if (credentials[0] == envUser && credentials[1] == envPass)
+            {
+                await next();
+                return;
+            }
+        }
+
+        context.Response.Headers["WWW-Authenticate"] = "Basic";
+        context.Response.StatusCode = 401;
+    }
+    else
+    {
+        await next();
+    }
+});
 
 app.MapHealthChecks("/health");
 
